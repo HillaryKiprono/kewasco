7d55b39e606cd766a5927c7302ab32aec2ebecb9
Kiprono <61239366+HillaryKiprono@users.noreply.github.com>
2023-09-14 03:14:08 -0700

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
-   // static const String ipAddress = "192.168.1.192";
-  static const String ipAddress = "192.168.1.9:8081";
+    static const String ipAddress = "192.168.1.192";
+ 
 }
